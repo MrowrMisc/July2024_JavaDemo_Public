46b2fe4306e074e0e15e1fdb36d0abdc42f445e2
Mrowr Purr <mrowr.purr@gmail.com>
2024-07-12 07:52:18 -0700

    Reply with identity for identify requests

--- java/websocket-server/src/main/kotlin/org/mrowrpurr/demo/websocketserver/WebsocketMessageHandler.kt
+++ java/websocket-server/src/main/kotlin/org/mrowrpurr/demo/websocketserver/WebsocketMessageHandler.kt
@@ -12,6 +12,7 @@ import org.springframework.web.socket.CloseStatus
 import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
+import java.util.UUID
 
 // Hardcoded secret hashes
 const val GAME_SECRET_HASH = "8956fd095458cfcaa4a6dec37a40e0a77707873f9ba4d74c543d0cefd54fa73f"
@@ -25,6 +26,11 @@ class WebsocketMessageHandler(
     private val lastMessageSent: LastMessageSent,
     private val jacksonObjectMapper: ObjectMapper
 ) : TextWebSocketHandler() {
+
+    private val uuidsToVerifiedRoles = mutableMapOf<UUID, String>()
+
+    private var gameSession: WebSocketSession? = null
+    private var adminSession: WebSocketSession? = null
     private val sessions = mutableListOf<WebSocketSession>()
 
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
@@ -33,7 +39,7 @@ class WebsocketMessageHandler(
         println("[MessageHandler] Publishing message to Redis: ${message.payload}")
         pubsub.convertAndSend("game", message.payload)
         println("[MessageHandler] Attempting to process message: ${message.payload}")
-        processMessageString(message.payload)
+        processMessageString(session, message.payload)
     }
 
     override fun afterConnectionEstablished(session: WebSocketSession) {
@@ -53,12 +59,12 @@ class WebsocketMessageHandler(
         sessions.forEach { it.sendMessage(TextMessage(message)) }
     }
 
-    private fun processMessageString(message: String) {
+    private fun processMessageString(session: WebSocketSession, message: String) {
         println("[MessageHandler] Processing message: $message")
         try {
             val jsonMapper = jacksonObjectMapper()
             val json: Map<String, Any> = jsonMapper.readValue(message)
-            processMessageMap(json)
+            processMessageMap(session, json)
         } catch (e: JsonParseException) {
             println("[MessageHandler] Error parsing JSON: $e")
         } catch (e: JsonMappingException) {
@@ -68,19 +74,28 @@ class WebsocketMessageHandler(
         }
     }
 
-    private fun processMessageMap(message: Map<String, Any>) {
+    private fun processMessageMap(session: WebSocketSession, message: Map<String, Any>) {
         println("[MessageHandler] Processing message: $message")
 
         val identifier = message.getOrDefault("identifier", null) as String?
         val action = message.getOrDefault("action", null) as String?
 
         if (identifier == null) {
-            if (action == "identify") actionIdentify(message)
+            if (action == "identify") actionIdentify(session, message)
             else println("[MessageHandler] Message from unidentified source: $message")
         }
     }
 
-    private fun actionIdentify(data: Map<String, Any>) {
+    private fun createNewIdentityAndReply(session: WebSocketSession, role: String) {
+        val uuid = UUID.randomUUID()
+        uuidsToVerifiedRoles[uuid] = role
+        val response = mutableMapOf<String, Any>()
+        response["status"] = "OK"
+        response["identifier"] = uuid.toString()
+        session.sendMessage(TextMessage(jacksonObjectMapper.writeValueAsString(response)))
+    }
+
+    private fun actionIdentify(session: WebSocketSession, data: Map<String, Any>) {
         println("[MessageHandler] Identifying: $data")
 
         val role = data.getOrDefault("role", null) as String?
@@ -102,6 +117,8 @@ class WebsocketMessageHandler(
             "game" -> {
                 if (secretHash == GAME_SECRET_HASH) {
                     println("[MessageHandler] Identified as game")
+                    gameSession = session
+                    createNewIdentityAndReply(session, role)
                 } else {
                     println("[MessageHandler] Invalid game secret")
                 }
@@ -109,6 +126,8 @@ class WebsocketMessageHandler(
             "admin" -> {
                 if (secretHash == ADMIN_SECRET_HASH) {
                     println("[MessageHandler] Identified as admin")
+                    adminSession = session
+                    createNewIdentityAndReply(session, role)
                 } else {
                     println("[MessageHandler] Invalid admin secret")
                 }
@@ -116,6 +135,7 @@ class WebsocketMessageHandler(
             "player" -> {
                 if (secretHash == PLAYER_SECRET_HASH) {
                     println("[MessageHandler] Identified as player")
+                    createNewIdentityAndReply(session, role)
                 } else {
                     println("[MessageHandler] Invalid player secret")
                 }
