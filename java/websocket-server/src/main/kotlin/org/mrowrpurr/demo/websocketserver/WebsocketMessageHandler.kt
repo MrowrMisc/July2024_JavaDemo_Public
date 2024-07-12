bc8b7bd79596a4334d901dba693259e7056b400a
Mrowr Purr <mrowr.purr@gmail.com>
2024-07-11 21:33:14 -0700

    Can send messages to ALL CONNECTED CLIENTS

--- java/websocket-server/src/main/kotlin/org/mrowrpurr/demo/websocketserver/WebsocketMessageHandler.kt
+++ java/websocket-server/src/main/kotlin/org/mrowrpurr/demo/websocketserver/WebsocketMessageHandler.kt
@@ -6,17 +6,24 @@ import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
 
 class WebsocketMessageHandler : TextWebSocketHandler() {
+    private val sessions = mutableListOf<WebSocketSession>()
+
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
         println("Received message: ${message.payload}")
+
+        // Send the message payload to ALL connected clients:
+        sessions.forEach { it.sendMessage(TextMessage("We received a message: ${message.payload}")) }
     }
 
     override fun afterConnectionEstablished(session: WebSocketSession) {
         super.afterConnectionEstablished(session)
         println("Connection established")
+        sessions.add(session)
     }
 
     override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
         super.afterConnectionClosed(session, status)
         println("Connection closed")
+        sessions.remove(session)
     }
 }
